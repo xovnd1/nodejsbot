abde6bd96b34ebae32f8a327cf88f6c205acf533
xovnd1 <lelttuel125@gmail.com>
2020-06-16 20:15:33 +0900

    add clear command

--- index.js
+++ index.js
@@ -106,8 +106,8 @@ client.on('message', (message) => {
     var clearLine = message.content.slice('!청소 '.length);
     var isNum = !isNaN(clearLine)
 
-    if(isNum && (clearLine <= 0 || 100 < clearLine)) {
-      message.channel.send("1부터 100까지의 숫자만 입력해주세요.")
+    if(isNum && (clearLine <= 0 || 500 < clearLine)) {
+      message.channel.send("1부터 500까지의 숫자만 입력해주세요.")
       return;
     } else if(!isNum) { // c @나긋해 3
       if(message.content.split('<@').length == 2) {
